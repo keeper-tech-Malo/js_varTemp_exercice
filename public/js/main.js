a13681c3d583a2568f8d416f04ae72c1ea314bc3
Malo <ndekezi37@gmail.com>
2021-01-05 10:57:56 +0100

    fin du travail

---
+++ public/js/main.js
@@ -0,0 +1,76 @@
+// let a = 1;
+// let b = 2
+// let c = 3
+
+// let temp;
+// temp = a
+// a = b
+// b = c
+// c = temp
+
+// console.log(a,b,c)
+
+// let a = 1;
+// let b = 2;
+// let c = 3; 
+// let d = 4;
+
+// let temp;
+// temp = a
+// tempb = b
+// a = d 
+// b = c
+// c = tempb
+// d = temp
+
+
+// console.log(a,b,c,d)
+
+// let a = 1; 
+// let b = 2;
+// let c = 3;
+// let d = 4;
+
+// let temp;
+// temp = a
+// a = b
+// b = c
+// c = d
+// d = temp
+
+// console.log(a,b,c,d)
+
+// let tableau = [1, 2, 3, 4];
+// temp = tableau[1]
+// tempx = tableau[0]
+// tableau[0] = tableau[3]
+// tableau[1] = tableau [2]
+// tableau[2] = temp
+// tableau[3] = tempx
+
+// console.log(tableau)
+
+
+let a = 1 
+let b = 2 
+let c = 3 
+let d = 4 
+let e = 5
+let f = 6
+
+let temp;
+temp = a
+tempd = b
+tempe = f
+tempf = c
+a = d 
+b = e
+c = temp
+d = tempd
+e = tempe
+f = tempf
+
+console.log(a,b,c,d,e,f)
+
+
+
